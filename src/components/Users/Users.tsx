14940d1e5f13cc4c8caa40efb6e7b5680fa49d11
Iq-Life <iq23life@gmail.com>
2021-02-10 10:44:02 +0300

    componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,14 +12,11 @@ export type UsersPropsType = {
 }
 
 export class Users extends React.Component <UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props)
-
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         })
     }
-
 render() {
     return <div className={style.all}>
         <div>
