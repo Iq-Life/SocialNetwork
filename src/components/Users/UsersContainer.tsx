9ae4fe0ee37f144e46f6db0781a38e2f3a9aec48
Iq-Life <iq23life@gmail.com>
2021-07-13 10:06:11 +0300

    hooks => useEffect()

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -56,18 +56,6 @@ class UsersContainer extends React.Component <UsersContainerPropsType> {
         </>
     }
 }
-/*
-let mapStateToProps = (state: AppStateType): MapStateToPropsType => {
-    return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress
-    }
-}
-*/
 
 let mapStateToProps = (state: AppStateType): MapStateToPropsType => {
     return {
