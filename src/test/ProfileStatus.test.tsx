f24ad90363e8217088f84ea4fc1fb71471e68023
Iq-Life <iq23life@gmail.com>
2021-07-27 20:40:13 +0300

    ProfileStatus.test.tsx

--- src/test/ProfileStatus.test.tsx
+++ src/test/ProfileStatus.test.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import {ProfileStatus} from "../components/Profile/ProfileStatus";
-import renderer from 'react-test-renderer';
+import {create} from 'react-test-renderer';
 
 
 
 describe('ProfileStatus component', () => {
     test('status from props.should be in the state', () => {
-        const component = renderer.create(<ProfileStatus status={"it-kamasutra.com"} updateStatusProfile={()=>{}}/>)
-        const instance = component.getInstance()
-    // @ts-ignore
-        expect(instance.state.status).toEqual("it-kamasutra.com");
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatusProfile={()=>{}}/>)
+        const root = component.root
+        expect(root.instance.state.status).toEqual("it-kamasutra.com")
+})
+    test('after creation <span> should be display', () => {
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatusProfile={()=>{}}/>)
+        const root = component.root
+        let span = root.findByType("span")
+        expect(span).not.toBeNull()
 });
+    test('after creation <input> should be display with correct status', () => {
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatusProfile={()=>{}}/>)
+        const root = component.root
+        expect(()=>{
+            let input = root.findByType("input")
+        }).toThrow()
 });
-describe('after creation <span> should be displayed', () => {
-    test('status from props.should be in the state', () => {
-        const component = renderer.create(<ProfileStatus status={"it-kamasutra.com"} updateStatusProfile={()=>{}}/>)
-        const instance = component.getInstance()
-    // @ts-ignore
-        expect(instance.state.status).toEqual("it-kamasutra.com");
+    test('after creation <span> should be display with correct status', () => {
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatusProfile={()=>{}}/>)
+        const root = component.root
+        let span = root.findByType("span")
+        expect(span.children[0]).toBe("it-kamasutra.com")
+});
+    test('input should be display in editMode instead of span', () => {
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatusProfile={()=>{}}/>)
+        const root = component.root
+        let span = root.findByType("span")
+        span.props.onDoubleClick()
+        let input = root.findByType("input")
+        expect(input.props.value).toBe("it-kamasutra.com")
 });
 });
