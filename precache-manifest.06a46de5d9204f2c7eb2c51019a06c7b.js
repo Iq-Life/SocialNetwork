8bddca23b71f325e1339a27d9ffe8b0dc5029ab2
Pavel <73390739+Iq-Life@users.noreply.github.com>
2021-10-19 10:51:03 +0300

    Updates

---
+++ precache-manifest.06a46de5d9204f2c7eb2c51019a06c7b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa20e4629cafced1d80e7242f197faef",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "e6866255c2e3f4af1427",
+    "url": "/SocialNetwork/static/css/main.fe5ddbbe.chunk.css"
+  },
+  {
+    "revision": "10f27ff32195f25a2712",
+    "url": "/SocialNetwork/static/js/2.460205b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.460205b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6866255c2e3f4af1427",
+    "url": "/SocialNetwork/static/js/main.06603a17.chunk.js"
+  },
+  {
+    "revision": "623e50c037e277f40291",
+    "url": "/SocialNetwork/static/js/runtime-main.e6c123d3.js"
+  },
+  {
+    "revision": "f32be7a197988ecabc6b51c71388bdb8",
+    "url": "/SocialNetwork/static/media/805.f32be7a1.svg"
+  },
+  {
+    "revision": "be951e71a940bcfba66f14bea724a69f",
+    "url": "/SocialNetwork/static/media/ava.be951e71.png"
+  },
+  {
+    "revision": "f90d8e7e0c1ca6d821e23b9bdbbeebac",
+    "url": "/SocialNetwork/static/media/head1.f90d8e7e.jpg"
+  },
+  {
+    "revision": "7b994355affd0d03db4a04527374788e",
+    "url": "/SocialNetwork/static/media/no.7b994355.png"
+  },
+  {
+    "revision": "935574cf382602a37767deef0732c3e0",
+    "url": "/SocialNetwork/static/media/yes.935574cf.png"
+  }
+]);
